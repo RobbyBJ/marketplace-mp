cae14e7a1906fd6c8dd698697152808b0b1e62bf
RobbyBJ <137489169+RobbyBJ@users.noreply.github.com>
2026-03-14 19:27:29 +0800

    vercel fix#5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,3 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// Vercel Support: Redirect storage to /tmp on serverless
+if (isset($_SERVER['VERCEL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
